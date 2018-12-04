426b7b03861f80b8dfb3fdf8be9d5c30494b4ac6
davidccgarcia <ccristhiangarcia@gmail.com>
2018-12-03 20:49:26 -0500

    WelcomeUsersTest

---
+++ tests/Feature/WelcomeUsersTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class WelcomeUsersTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_welcomes_users_with_nickname()
+    {
+        $this->get('users/david/mono')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido David, tu apodo es mono');
+    }
+
+    /**
+     * @test
+     */
+    public function it_welcomes_users_without_nickname()
+    {
+        $this->get('users/david')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido David');
+    }
+}
