f5fdad84b12c74685b90eed6f07e6e96e1f11584
davidccgarcia <ccristhiangarcia@gmail.com>
2018-12-04 09:49:20 -0500

    Create an action to greet a user by name and another to greet a user by nickname

--- app/Http/Controllers/WelcomeUserController.php
+++ app/Http/Controllers/WelcomeUserController.php
@@ -6,14 +6,15 @@ use Illuminate\Http\Request;
 
 class WelcomeUserController extends Controller
 {
-    public function __invoke($name, $nickname = null)
+    public function welcomeWithNickname($nickname)
+    {
+        return "Bienvenido {$nickname}";
+    }
+
+    public function welcomeWithName($name)
     {
         $name = ucfirst($name);
 
-        if ($nickname) {
-            return "Bienvenido {$name}, tu apodo es {$nickname}";
-        } else {
-            return "Bienvenido {$name}";
-        }
+        return "Bienvenido {$name}";
     }
 }
