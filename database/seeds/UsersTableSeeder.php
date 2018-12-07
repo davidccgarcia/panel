e1025684c73d8cd2c7b156e98ede4770ac6e4f33
davidccgarcia <ccristhiangarcia@gmail.com>
2018-12-07 00:03:47 -0500

    SQL Laravel Query Builder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        // $professions = DB::select('SELECT id FROM professions WHERE title = :title LIMIT 0,1', ['title' => 'Desarrollador back-end']);
+
+        $professionId = DB::table('professions')
+            ->whereTitle('Desarrollador back-end')
+            ->value('id');
+
         DB::table('users')->insert([
+            'profession_id' => $professionId,
             'name' => 'David Garcia',
             'email' => 'ccristhiangarcia@gmail.com',
             'password' => bcrypt('secret')
