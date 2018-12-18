3a3bfe7bdfd09c970307b95f1261ceb58f1ec237
davidccgarcia <ccristhiangarcia@gmail.com>
2018-12-18 16:48:24 -0500

    Create form to add users

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,6 +6,16 @@
     <h1>Crear usuarios</h1>
     <form action="{{ route('users.store') }}" method="POST">
         {{ csrf_field() }}
+        <label for="name">Nombre Completo</label>
+        <input type="text" name="name" id="name" placeholder="Pablo Perez">
+        <br>
+
+        <label for="email">Correo electronico</label>
+        <input type="email" name="email" id="email" placeholder="pperez@example.com">
+        <br>
+
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" placeholder="Minimo 6 caracteres">
         <button type="submit" class="btn btn-info">Create user</button>
     </form>
 @endsection
