78af7438b3b71d50d9e0f515b526cb366b56278d
davidccgarcia <ccristhiangarcia@gmail.com>
2018-12-19 09:37:56 -0500

    Show validation errors and preserve form data

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,14 +4,28 @@
 
 @section('content')
     <h1>Crear usuarios</h1>
+
+    {{-- @if ($errors->any())
+        <div class="alert alert-danger">
+            <p>Por favor corrige los siguientes errores: </p>
+            <ul>
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif --}}
     <form action="{{ route('users.store') }}" method="POST">
         {{ csrf_field() }}
         <label for="name">Nombre Completo</label>
         <input type="text" name="name" id="name" placeholder="Pablo Perez">
+        @if ($errors->any)
+            <p>{{ $errors->first('name') }}</p>
+        @endif
         <br>
 
         <label for="email">Correo electronico</label>
-        <input type="email" name="email" id="email" placeholder="pperez@example.com">
+        <input type="email" name="email" id="email" placeholder="pperez@example.com" value="{{ old('email') }}">
         <br>
 
         <label for="password">Contraseña</label>
