e9a84f0f8d7b331b7fbfb79a297819ae3049ecad
David García <ccristhiangarcia@gmail.com>
2019-07-21 22:44:16 -0500

    Save a register in multiples tables with Eloquent and Laravel

--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -87,13 +87,21 @@ class UsersModuleTest extends TestCase
         $this->post('users', [
             'name' => 'David Garcia',
             'email' => 'ccristhiangarcia@gmail.com',
-            'password' => '123456'
+            'password' => '123456',
+            'bio' => 'Desarrollador de Laravel y Vue.js',
+            'twitter' => 'https://twitter.com/davidccgarcia'
         ])->assertRedirect(route('users.store'));
 
         $this->assertCredentials([
             'name' => 'David Garcia',
             'email' => 'ccristhiangarcia@gmail.com',
             'password' => '123456'
+        ]);
+
+        $this->assertDatabaseHas('user_profiles', [
+            'bio' => 'Desarrollador de Laravel y Vue.js',
+            'twitter' => 'https://twitter.com/davidccgarcia',
+            'user_id' => User::findByEmail('ccristhiangarcia@gmail.com')->id,
         ]);
     }
 
